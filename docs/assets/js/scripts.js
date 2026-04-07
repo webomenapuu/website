ee9219d7ec57b8e3a44dacfa231b6c23655a31e8
webomenapuu <web.omenapuu@gmail.com>
2026-04-07 08:38:05 +0300

    Use relative paths in css

--- docs/assets/js/scripts.js
+++ docs/assets/js/scripts.js
@@ -95,16 +95,18 @@ window.addEventListener('DOMContentLoaded', event => {
 
     // Promo modal
     var modal = document.getElementById("promo");
-    var close = document.getElementsByClassName("close")[0];
-    modal.style.display = "block";
+    if (modal) {
+        var close = document.getElementsByClassName("close")[0];
+        modal.style.display = "block";
 
-    close.onclick = function () {
-        modal.style.display = "none";
-    }
-
-    window.onclick = function (event) {
-        if (event.target == modal) {
+        close.onclick = function () {
             modal.style.display = "none";
+        }
+
+        window.onclick = function (event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
         }
     }
 });
